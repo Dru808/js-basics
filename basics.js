b7895efb0901d1fa5ed7656015022ec875ddddd0
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-13 16:37:38 -1000

    finished problem 3

--- basics.js
+++ basics.js
@@ -8,17 +8,33 @@
 
 var person = {
 	"name": "whaa?",
-	"age": 6
+	"age": 16
 };
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
 
+ var canDrive = null;
+
+if (person.age >= 16){
+	canDrive = true;
+
+}else {
+	canDrive = false;
+}
+
+
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+
+function greet(name){
+	console.log("Hello, my name is " + name);
+	
+}
+greet("whaa?");
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
